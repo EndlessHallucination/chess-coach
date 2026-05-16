49f7e22b43d6fa672ae2da319ac2cc9cb8e08aac
basil <basiln@icloud.com>
2026-05-16 19:21:23 +0300

    add game loop wth turns

--- main.py
+++ main.py
@@ -2,5 +2,28 @@ import chess
 
 board = chess.Board()
 
+
+while not board.is_game_over():
+    print(board)
+    print()
+
+    print("White to move" if board.turn == chess.WHITE else "Black to move")
+    
+    move = input("Type your move or 'quit'>> ")
+
+    if move == 'quit':
+        break
+    
+    try: 
+        board.push_san(move)
+    except ValueError:
+        print("Illegal move! Try again")
+
 print(board)
-print("White to move" if board.turn == chess.WHITE else "Black to move")
+
+if board.is_checkmate():
+    winner = "Black" if board.turn == chess.WHITE else "White"
+
+    print(f"Checkmate!, {winner} wins!")
+else: 
+    print("Stalemate")
